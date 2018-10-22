2b176092290ca6952243b0b84f6477a54f84d390
xingjiudong <25635680@qq.com>
2018-10-22 12:24:24 +0800

    update lam.conf

--- setup.sh
+++ setup.sh
@@ -18,4 +18,6 @@ if [ ! -f "$LAM_CONF_FILE" ]; then
     sed -i "s/types: suffix_group: ou=group,dc=my-domain,dc=com/types: suffix_group: ${GROUP_DN}/g" ${LAM_CONF_FILE}
 fi
 
+chown www-data:www-data ${LAM_CONF_FILE}
+
 exec /usr/local/bin/apache2-foreground
